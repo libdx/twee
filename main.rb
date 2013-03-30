da9cfa6334ee10ca32903582c702bae6fda4e543
Alex <dx@dundy.dundy>
2013-03-30 23:24:09 +0200

    Wrap all in module Twee

---
+++ main.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'gtk2'
+require 'login'
+
+module Twee
+	class App
+		def run
+			@login = Login.new(
+				:on_ok => lambda{puts :sign_in},
+				:on_cancel => lambda{puts :now_way}
+			)
+			@login.dialog.signal_connect("destroy") {Gtk.main_quit}
+			@login.dialog.show_all
+			Gtk.main
+		end
+	end
+end
+
+Twee::App.new.run
+
---
+++ model.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+module Twee
+	class User
+		attr_accessor :username
+		attr_accessor :password
+		attr_accessor :token
+	end
+end
+
